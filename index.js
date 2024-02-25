3242692ac6605fa09ea8a20f5464ef9e2dfa2228
Alexandra Busson <alexandra_b@live.fr>
2024-02-25 17:58:42 +0100

    new folder for img, adding elements to give correct positions

---
+++ index.js
@@ -0,0 +1,10 @@
+const startBtn = document.querySelector("#start-game");
+const musicBtn = document.querySelector("#music")
+
+startBtn.addEventListener("click", () => {
+    console.log("clicked");
+})
+
+musicBtn.addEventListener("click", () => {
+    console.log("clicked");
+})
